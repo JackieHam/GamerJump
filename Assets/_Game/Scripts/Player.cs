238730fc4a1bd74a85022197f70f4fba0fe61dda
Cameron <cameo221@gmail.com>
2025-07-12 20:41:07 -0700

    script stuff

--- Assets/_Game/Scripts/Player.cs
+++ Assets/_Game/Scripts/Player.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 
-public class Player : MonoBehaviour
+namespace _Game.Scripts
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class Player : MonoBehaviour
     {
+        public Rigidbody2D
         
-    }
+        private void Start()
+        {
+        
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        private void Update()
+        {
         
+        }
     }
 }
